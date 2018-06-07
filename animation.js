4b996024c290ff3bf9c0c0827530357fb9c9748c
Rocsana E De Leon-Cifuentes <rd49373@srcs.loc>
2018-06-07 11:11:20 -0700

    Not too sure

--- animation.js
+++ animation.js
@@ -1,20 +1,16 @@
-var box = document.getElementById("text");
+var p = document.getElementById("text");
 var xpos = 0;
 var ypos = 0;
-var xstep = 1;
-var ystep = 1;
 var id = setInterval(frame, 5);
 
 function frame() {
-    if (xpos > 350 || xpos < 0) {
-        xstep = -xstep;
+    if (xpos == 350) {
+        clearInterval(id);
     }
-     if (ypos > 350 || ypos < 0) {
-        ystep = -ystep;
+    else {
+        xpos++;
+        ypos++;
+        triangle.style.top = ypos + 'px';
+        triangle.style.left = xpos + 'px';
     }
-
-    xpos = xpos + xstep;
-    ypos = ypos + ystep;
-    box.style.top = ypos + 'px';
-    box.style.left = xpos + 'px';
 }
